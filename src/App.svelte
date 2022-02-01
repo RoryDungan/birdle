86804cd85440acf7444768d9d3a362b7dd79b1f7
Rory Dungan <r_dungan@wargaming.net>
2022-02-01 15:13:02 +1100

    Initial commit based on Svelte template

---
+++ src/App.svelte
@@ -0,0 +1,64 @@
+<script>
+  import WorldGrid from './WordGrid.svelte'
+
+  const answer = 'goose'
+  const wordList = [
+    'eagle',
+    'chook',
+    'goose',
+    'booby',
+    'stork',
+    'drake',
+    'finch',
+    'swift',
+    'heron',
+    'robin',
+    'raven',
+    'doves',
+    'ducks',
+    'crows',
+    'wrens',
+    'hawks',
+    'larks',
+    'terns',
+    'gulls',
+    'serin',
+    'grebe',
+    'owlet',
+    'pipit',
+    'pitta',
+    'snipe',
+    'hobby',
+    'crake',
+  ]
+  const guesses = []
+  $: guessedLetters = new Set(guesses.join('').split(''))
+</script>
+
+<main>
+  <h1>Birdle</h1>
+  <p>Guess the 5 letter bird</p>
+  <WorldGrid />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
